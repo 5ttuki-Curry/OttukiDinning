35a5875615216c72bab0360f19aa5322ec84abe9
신승현 <sinseunghyeon313@gmail.com>
2024-04-22 15:13:30 +0900

    [Feat] 주석 기능 추가 #1

--- OttukiDinning/ViewController.swift
+++ OttukiDinning/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // 주석 추가
     }
 
 
