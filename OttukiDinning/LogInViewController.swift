8e2d0b72a95dfa965f507b033da0c8ba93027ccf
박준영 <labydin@naver.com>
2024-04-22 17:08:49 +0900

    [Add] color assets 추가 #6

--- OttukiDinning/LogInViewController.swift
+++ OttukiDinning/LogInViewController.swift
@@ -19,7 +19,7 @@ class LogInViewController: UIViewController {
     
     @IBOutlet weak var signUpButton: UIButton!
     
-    // 카카오로 시작 버튼은 스토리보드에 만들어만 둠, 추가 구현 의논시 결정
+    // 카카오로 시작 버튼은 스토리보드에 만들어만 둠, 추가 구현할 지 추후 결정
     
     
     override func viewDidLoad() {
@@ -30,14 +30,24 @@ class LogInViewController: UIViewController {
     
     
     func configureUI() {
+        emailTextField.layer.cornerRadius = 20
+        emailTextField.placeholder = "이메일"
+        emailTextField.borderStyle = .roundedRect
+        
+        passwordTextField.layer.cornerRadius = 20
+        passwordTextField.placeholder = "패스워드"
+        
+        logInButton.backgroundColor = 
+        logInButton.layer.cornerRadius = 20
+        
         
     }
     
     @IBAction func logInButtonTapped(_ sender: UIButton) {
-        
+        // tap bar 첫번째 화면으로 이동
     }
     
     @IBAction func signUpButtonTapped(_ sender: UIButton) {
-        
+        // 회원가입 화면으로 이동
     }
 }
