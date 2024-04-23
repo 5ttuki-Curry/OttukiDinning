b86faa06985e0c0f6eeb8f5a5a05b37e074e0719
신승현 <sinseunghyeon313@gmail.com>
2024-04-23 20:05:26 +0900

    [Design] 메인페이지 CollectionView 추가 #5

--- OttukiDinning/HomeViewController.swift
+++ OttukiDinning/HomeViewController.swift
@@ -7,48 +7,64 @@
 
 import UIKit
 
-class HomeViewController: UICollectionViewController, UISearchResultsUpdating {
+class HomeViewController: UICollectionViewController {
     
     var bottomUIStackView = UIStackView()
     var homeImage = UIImage(named: "Home")
     var searchImage = UIImage(named: "Search")
     var myInfoImage = UIImage(named: "MyInfo")
-    var searchController: UISearchController!
-    let searchBar = UISearchBar()
+    var profileImage = UIImage(named: "Profile")
+    
+    let profileButton = UIButton()
+    let topSearchButton = UIButton()
+    let homeButton = UIButton()
+    let searchButton = UIButton()
+    let myInfoButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setBottomUIStackView()
-//        self.setCollectionView()
-        self.setSearchController()
-    }
-    
-    private func setSearchController() {
-        // UISearchController 초기화 및 설정
-        searchController = UISearchController(searchResultsController: nil)
-        searchController.searchResultsUpdater = self
-        searchController.obscuresBackgroundDuringPresentation = false
-        searchController.searchBar.placeholder = "검색어를 입력해주세요" // 검색창에 표시될 텍스트 설정
-        navigationItem.searchController = searchController
-        definesPresentationContext = true
-    }
-    
-    func updateSearchResults(for searchController: UISearchController) {
-        // 검색 결과 업데이트 로직 구현
-        guard let searchText = searchController.searchBar.text else {
-            return
-        }
-        
-        print(searchText)
+        self.setTopUIButton()
+        self.setCollectionView()
+        view.backgroundColor = .white
     }
     
     private func setCollectionView() {
+        
+        let layout = UICollectionViewFlowLayout()
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(UINib(nibName: "HomeCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "HomeCollectionViewCell")
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.topAnchor.constraint(equalTo: topSearchButton.bottomAnchor),
             collectionView.bottomAnchor.constraint(equalTo: bottomUIStackView.topAnchor),
             collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+    }
+    
+    private func setTopUIButton() {
+        
+        view.addSubview(profileButton)
+        view.addSubview(topSearchButton)
+        
+        profileButton.setImage(profileImage, for: .normal)
+        topSearchButton.setImage(searchImage, for: .normal)
+        
+        profileButton.translatesAutoresizingMaskIntoConstraints = false
+        topSearchButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            profileButton.heightAnchor.constraint(equalToConstant: 45),
+            profileButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            profileButton.widthAnchor.constraint(equalToConstant: 45),
+            profileButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+            topSearchButton.heightAnchor.constraint(equalToConstant: 45),
+            topSearchButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            topSearchButton.widthAnchor.constraint(equalToConstant: 45),
+            topSearchButton.trailingAnchor.constraint(equalTo: profileButton.leadingAnchor, constant: -10)
         ])
     }
     
@@ -58,14 +74,10 @@ class HomeViewController: UICollectionViewController, UISearchResultsUpdating {
         bottomUIStackView.distribution = .equalSpacing
         bottomUIStackView.isLayoutMarginsRelativeArrangement = true
         bottomUIStackView.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 0, leading: 39, bottom: 0, trailing: 39)
-
         
-        let homeButton = UIButton(type: .system)
-        homeButton.setImage(homeImage?.withRenderingMode(.alwaysOriginal), for: .normal)
-        let searchButton = UIButton(type: .system)
-        searchButton.setImage(searchImage?.withRenderingMode(.alwaysOriginal), for: .normal)
-        let myInfoButton = UIButton(type: .system)
-        myInfoButton.setImage(myInfoImage?.withRenderingMode(.alwaysOriginal), for: .normal)
+        homeButton.setImage(homeImage, for: .normal)
+        searchButton.setImage(searchImage, for: .normal)
+        myInfoButton.setImage(myInfoImage, for: .normal)
         
         bottomUIStackView.addArrangedSubview(homeButton)
         bottomUIStackView.addArrangedSubview(searchButton)
@@ -80,5 +92,45 @@ class HomeViewController: UICollectionViewController, UISearchResultsUpdating {
         ])
     }
     
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HomeCollectionViewCell", for: indexPath) as? HomeCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        switch indexPath.row {
+        case 0:
+            cell.titleLabel.text = "지역별 랜덤 추천 메뉴"
+        case 1:
+            cell.titleLabel.text = "RISING PLACE"
+        case 2:
+            cell.titleLabel.text = "HOT PLACE"
+        default:
+            cell.titleLabel.text = "Error"
+        }
+        
+        return cell
+    }
 }
 
+extension HomeViewController: UICollectionViewDelegateFlowLayout {
+    // UICollectionViewDelegateFlowLayout 메소드
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        // 화면의 너비
+        let width = collectionView.frame.width
+        // 화면의 높이를 3으로 나누어 각 섹션의 높이를 설정
+        let height = collectionView.frame.height / 3
+        
+        return CGSize(width: width, height: height)
+    }
+}
