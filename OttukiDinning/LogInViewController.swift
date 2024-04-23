0c10787e7d8167e1a5740bc72fb8362309854dfd
박준영 <labydin@naver.com>
2024-04-23 13:21:20 +0900

    [Feat] 키보드 return키 클릭 시 이벤트 기능 추가

--- OttukiDinning/LogInViewController.swift
+++ OttukiDinning/LogInViewController.swift
@@ -20,11 +20,17 @@ class LogInViewController: UIViewController {
     
     // 카카오로 시작 버튼은 스토리보드에 만들어만 둠, 추가 구현할 지 추후 결정
     
+    let defaults = UserDefaults.standard
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        idTextField.delegate = self
+        passwordTextField.delegate = self
+        
         configureUI()
+        
     }
     
     
@@ -34,14 +40,19 @@ class LogInViewController: UIViewController {
         idTextField.layer.cornerRadius = 15
         idTextField.placeholder = "이메일"
         idTextField.keyboardType = .emailAddress
+        idTextField.returnKeyType = .next
         idTextField.clearButtonMode = .always
+        idTextField.text = readLoginInfo(forKey: "id")
         
         passwordTextField.layer.borderWidth = 1
         passwordTextField.layer.borderColor = UIColor(named: "ShadowColor")?.cgColor
         passwordTextField.layer.cornerRadius = 15
         passwordTextField.placeholder = "패스워드"
         passwordTextField.keyboardType = .emailAddress
+        passwordTextField.returnKeyType = .done
         passwordTextField.clearButtonMode = .always
+        passwordTextField.isSecureTextEntry = true
+        passwordTextField.text = readLoginInfo(forKey: "password")
         
         logInButton.layer.borderWidth = 1
         logInButton.layer.borderColor = UIColor(named: "ShadowColor")?.cgColor
@@ -58,7 +69,15 @@ class LogInViewController: UIViewController {
     
     
     @IBAction func logInButtonTapped(_ sender: UIButton) {
-        // tap bar 첫번째 화면으로 이동
+        // id, pw 맞는지 확인
+        if idTextField.text == readLoginInfo(forKey: "id"), passwordTextField.text == readLoginInfo(forKey: "password") {
+            //  맞으면 다음 화면으로 이동
+            print("다음화면 이동")
+        } else {
+            // 틀리면 alert창 띄워 경고
+            _ = UIAlertController(title: "가입하신 이메일 또는 비밀번호가 아닙니다.", message: "올바르게 입력해 주세요.", preferredStyle: .alert)
+        }
+        
     }
     
     
@@ -69,9 +88,36 @@ class LogInViewController: UIViewController {
         
     }
     
+    
+    func readLoginInfo(forKey: String) -> String {
+        if let value = defaults.string(forKey: forKey) {
+            return value
+        } else { return "" }
+    }
+    
+    
+   
+
 }
 
 
 extension LogInViewController: UITextFieldDelegate {
-     
+    
+    //빈 화면 터치하면 키보드 내리기
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    
+    // 엔터 누르면 키보드 내리기
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == idTextField {
+            passwordTextField.becomeFirstResponder()
+        } else if textField == passwordTextField {
+            textField.resignFirstResponder()
+        }
+        
+        return true
+    }
 }
+
--- OttukiDinning/SignUpViewController.swift
+++ OttukiDinning/SignUpViewController.swift
@@ -16,6 +16,7 @@ class SignUpViewController: UIViewController {
     
     // 카카오로 시작 버튼 구현 여부는 추후 결정
     
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,16 +31,21 @@ class SignUpViewController: UIViewController {
         emailTextField.layer.cornerRadius = 15
         emailTextField.placeholder = "이메일 주소를 입력하세요"
         emailTextField.keyboardType = .emailAddress
+        emailTextField.returnKeyType = .next
         emailTextField.clearButtonMode = .always
         
         setPasswordTextField.layer.borderWidth = 1
         setPasswordTextField.layer.borderColor = UIColor(named: "ShadowColor")?.cgColor
         setPasswordTextField.layer.cornerRadius = 15
+        emailTextField.keyboardType = .emailAddress
+        emailTextField.returnKeyType = .next
         setPasswordTextField.placeholder = "패스워드를 입력하세요"
         
         checkPasswordTextField.layer.borderWidth = 1
         checkPasswordTextField.layer.borderColor = UIColor(named: "ShadowColor")?.cgColor
         checkPasswordTextField.layer.cornerRadius = 15
+        checkPasswordTextField.keyboardType = .emailAddress
+        checkPasswordTextField.returnKeyType = .done
         checkPasswordTextField.placeholder = "패스워드를 한 번 더 입력하세요"
         
         makeAccountButton.layer.borderWidth = 1
@@ -51,5 +57,14 @@ class SignUpViewController: UIViewController {
         makeAccountButton.titleLabel?.font = .boldSystemFont(ofSize: 18)
     }
     
-
+    
+    @IBAction func makcAccountButtonTapped(_ sender: UIButton) {
+        // 데이터(이메일, 패스워드) 저장하기
+        defaults.set(emailTextField.text, forKey: "id")
+        defaults.set(setPasswordTextField.text, forKey: "password")
+        
+        // 로그인 화면으로 돌아가기
+        self.dismiss(animated: true, completion: nil)
+    }
+    
 }
