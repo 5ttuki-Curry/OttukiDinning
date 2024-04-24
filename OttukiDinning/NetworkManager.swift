0dcecccc1cb4ae22fa0196cd976e55cc94a53b65
신승현 <sinseunghyeon313@gmail.com>
2024-04-25 00:49:54 +0900

    Revert "Merge branch 'FeatureAPI' into develop"
    This reverts commit 8ae0b45eeb7fe1dcd3b515810d7e61e83b948b6c, reversing
    changes made to fc09ae2b07e36e29e7a9126cf1b5a58755575f57.

--- OttukiDinning/NetworkManager.swift
+++ OttukiDinning/NetworkManager.swift
@@ -5,38 +5,26 @@
 //  Created by t2023-m0056 on 4/22/24.
 //
 
+import Alamofire
 import Foundation
 
 class NetworkManager {
     
-    func searchPlaces(keyword: String) {
-        let apiKey = "4457647991508438bc027a5a8e998987" // REST API 키
-        let urlString = "https://dapi.kakao.com/v2/local/search/keyword.json?query=\(keyword)".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+    // Alamofire를 이용한 키워드 식당 리스트
+    func searchRestaurantList(keyword: String, completion: @escaping (Result<[RestaurantData], Error>) -> Void) {
+        let headers: HTTPHeaders = ["Authorization": "KakaoAK 4457647991508438bc027a5a8e998987"]
+        let parameters: Parameters = ["query": keyword]
+        let url = "https://dapi.kakao.com/v2/local/search/keyword.json"
         
-        guard let url = URL(string: urlString) else {
-            return
-        }
-
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        request.addValue("KakaoAK \(apiKey)", forHTTPHeaderField: "Authorization")
-
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            guard let data = data, error == nil else {
-                print("Error: \(error?.localizedDescription ?? "Unknown error")")
-                return
-            }
-            
-            do {
-                // JSON 데이터 처리
-                if let jsonResult = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
-                    print(jsonResult)
-                    // 필요한 정보 추출 및 사용
-                }
-            } catch let error {
-                print("JSON 변환 실패: \(error.localizedDescription)")
+        AF.request(url, method: .get, parameters: parameters, headers: headers).validate().responseDecodable(of: KakaoAPIResponse.self) { response in
+            switch response.result {
+            case .success(let responseData):
+                completion(.success(responseData.documents))
+            case .failure(let error):
+                print("Error: 데이터 받아오기 실패, \(error.localizedDescription)")
+                completion(.failure(error))
             }
         }
-        task.resume()
     }
+    
 }
