a81e6bbc81f1f12637a1ef77203616f68dd98240
tldhs27 <162525811+tldhs27@users.noreply.github.com>
2024-04-26 15:53:27 +0900

    [Sion] 서치바 및 결과 구현중

--- OttukiDinning/ListCell.swift
+++ OttukiDinning/ListCell.swift
@@ -15,6 +15,27 @@ class ListCell: UICollectionViewCell {
     @IBOutlet weak var ratingLabel: UILabel!
     @IBOutlet weak var cellImage: UIImageView!
     
+    private var searchStore : RestaurantData? = nil {
+        didSet {
+            guard let searchStore = self.searchStore else {return}
+            
+            // 메인 페이지 상품 데이터 JSON Dummy API 활용해서 노출하기  (썸네일, 상품명, 설명, 가격)
+            DispatchQueue.main.async {
+//이미지                self.cellImage.image = nil
+                self.nameLabel.text = searchStore.placeName
+                self.addressLabel.text = searchStore.addressName
+//별점                self.ratingLabel.text =
+            }
+            
+            // 이미지
+//            DispatchQueue.global().async { [weak self] in
+//                if let data = try? Data(contentsOf: RestaurantData.image), let image = UIImage(data: data) {
+//                    DispatchQueue.main.async { self?.cellImage.image = image}
+//                }
+//            }
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
@@ -27,22 +48,22 @@ class ListCell: UICollectionViewCell {
             contentView.backgroundColor = UIColor(red: 255/255, green: 247/255, blue: 176/255, alpha: 0.3)
             
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.3){
-                self.contentView.backgroundColor = .clear
+                self.contentView.backgroundColor = .white
             }
         }
     }
     
-    func setCell(_ data: SearchStoreData){
+    func setCell(_ data: RestaurantData){
         
-        if let image = data.image {
-            cellImage.image = image
-        }
+// 이미지       if let image = data.image {
+//            cellImage.image = image
+//        }
         
-        nameLabel.text = data.name
+        nameLabel.text = data.placeName
         nameLabel.layer.masksToBounds = true
-        addressLabel.text = data.address
+        addressLabel.text = data.addressName
         addressLabel.layer.masksToBounds = true
-        ratingLabel.text = String(data.rate)
+// 별점      ratingLabel.text = String(data.rate)
         
         
         // cell 테두리
@@ -63,7 +84,39 @@ class ListCell: UICollectionViewCell {
         contentView.layer.masksToBounds = false
     }
     
-    
+    //URL Session
+    private func fetchRemoteProduct() {
+        let storeID = Int.random(in: 1...100) // 1~100까지 랜덤한 숫자
+        
+        if let url = URL(string: "https://dummyjson.com/products/\(storeID)") {
+            // URLSessionDataTask를 사용하여 비동기적으로 데이터 요청
+            let task = URLSession.shared.dataTask(with: url) { data, response, error in
+                if let error = error {
+                    print("Error: \(error)")
+                }
+                else if let data = data {
+                    
+                    do {
+                        let store = try JSONDecoder().decode(RestaurantData.self, from: data)
+                        self.searchStore = store
+                        
+                        print(store.placeName)
+                        print(store.addressName)
+// 이미지                       print(store.image)
+                        
+                    } catch {
+                        print("Decode Error: \(error)")
+                    }
+                }
+                guard let response = response as? HTTPURLResponse,
+                      (200...299).contains(response.statusCode)  // 보통 성공한 응답은 200번대임.
+                else {return}
+            }
+            
+            // 네트워크 요청 시작
+            task.resume()
+        }
+    }
 
     
 }
